6277fe2ef650ecdf41f24eda3c2f454ba152891c
Rangga008 <rizk.rangga09@gmail.com>
2024-10-14 11:42:01 +0700

    Update server.js

--- server.js
+++ server.js
@@ -10,23 +10,18 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware to parse JSON
 app.use(bodyParser.json());
 
-// Serve static files from the "public" directory
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "public")));
 
-// Use product routes
 app.use("/api", productRoutes);
 
-// Root route
 app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// Start the server
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 });
